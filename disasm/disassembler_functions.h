311a45aba19f362feb23533fba46b06abdbd6f97
Asami Tachibana <atachibana2612@gmail.com>
2017-09-28 16:27:06 -0400

    Moved the invaders ROM to the disassembler directory, for ease of use.

--- disasm/disassembler_functions.h
+++ disasm/disassembler_functions.h
@@ -17,18 +17,18 @@ int DA8080_Op(unsigned char *codebuffer, int pc)
   switch (*code)
   {
     case 0x00: printf("NOP  "); break;
-    case 0x01: printf("LXI  B , #$%02x%02x", code[2], code[1]); opbytes=3; break;
-    case 0x02: printf("STAX B "); break;
-    case 0x03: printf("INX  B "); break;
-    case 0x04: printf("INR  B "); break;
+    case 0x01: printf("LXI	B , #$%02x%02x", code[2], code[1]); opbytes=3; break;
+    case 0x02: printf("STAX	B"); break;
+    case 0x03: printf("INX	B "); break;
+    case 0x04: printf("INR	B "); break;
     case 0x05: printf("DCR  B "); break;
-    case 0x06: printf("MVI  B ,#$%02x", code[1]); opbytes=2; break;
+    case 0x06: printf("MVI B,	#$%02x", code[1]); opbytes=2; break;
     case 0x07: printf("RLC  "); break;
     case 0x08: printf("NOP  "); break;
-    case 0x09: printf("DAD  B "); break;
-    case 0x0a: printf("LDAX B "); break;
-    case 0x0b: printf("DCX  B "); break;
-    case 0x0c: printf("INR  C "); break;
+    case 0x09: printf("DAD	B"); break;
+    case 0x0a: printf("LDAX	B"); break;
+    case 0x0b: printf("DCX	B"); break;
+    case 0x0c: printf("INR	C "); break;
     case 0x0d: printf("DCR  C "); break;
     case 0x0e: printf("MVI  C,  #$%02x", code[1]); opbytes=2; break;
     case 0x0f: printf("RRC  "); break;
