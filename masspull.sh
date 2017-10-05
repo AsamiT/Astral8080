3cb56df90f09b9b9c99a5bd08991b391a95765f8
Asami Tachibana <atachibana2612@gmail.com>
2017-10-05 15:53:09 -0400

    Added by auto-script

--- masspull.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -L1 sh -c 'cd "$0" && pwd && git pull'
-
--- masspush.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -L1 sh -c 'cd "$0" && pwd && git add . && git commit -m "Added by auto-script" && git push'
-
